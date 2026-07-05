1461783c6f7c5fc9def54f6f14c391204a6d9d21
Vadim Kurenev <160814841+Vadim0-0@users.noreply.github.com>
2026-07-05 20:12:52 +0500

    feat(web): add footer component and scroll-to-top button to default layout

---
+++ web/app/components/layout/topButton/TopButton.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+  const lenis = useLenis();
+
+  function scrollToTop() {
+    lenis.value?.scrollTo(0, {
+      duration: 4,
+    })
+  };
+</script>
+
+<template>
+  <button 
+    @click="scrollToTop"
+    class="
+      btn-global fixed bottom-10 right-10 flex items-center justify-center p-0 w-15 h-15 rounded-[50%] z-100
+      max-md:bottom-5 max-md:right-5 max-md:w-10 max-md:h-10 
+    "
+  >
+    <Icon 
+      name="tabler:arrow-up" 
+      class="
+        bg-main size-9
+        max-md:size-6
+      "
+    />
+  </button>
+</template>
--- web/app/layouts/default.vue
+++ web/app/layouts/default.vue
@@ -1,4 +1,10 @@
-<script>
+<script setup lang="ts">
+
+  const isTopButtonVisible = ref(false)
+
+  useLenis(({ scroll, limit }: { scroll: number; limit: number }) => {
+    isTopButtonVisible.value = scroll + window.innerHeight >= limit + 100
+  })
 
 </script>
 
@@ -6,4 +12,24 @@
   <main class="flex flex-col flex-1">
     <slot />
   </main>
+  
+  <LayoutAppIndexFooter />
+
+  <Transition name="top-button">
+    <LayoutTopButton v-if="isTopButtonVisible" />
+  </Transition>
 </template>
+
+<style>
+
+  .top-button-leave-active,
+  .top-button-enter-active {
+    transition: opacity 0.3s ease-in-out,
+  }
+    
+  .top-button-enter-from,
+  .top-button-leave-to {
+    opacity: 0;
+  }
+
+</style>
