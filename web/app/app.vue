055b2668c8d42415f1b33bf9398c0098de80aa1f
Vadim Kurenev <160814841+Vadim0-0@users.noreply.github.com>
2026-07-08 09:40:45 +0500

    feat(web): add global loader

--- web/app/app.vue
+++ web/app/app.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+  const auth = useAuthStore()
+</script>
+
 <template>
   <VueLenis root :options="{ duration: 1.4, smoothWheel: true }" />
   <NuxtLayout>
@@ -5,4 +9,20 @@
   </NuxtLayout>
 
   <LayoutNotifications />
+
+  <Transition name="loader">
+    <LayoutLoader v-if="auth.isLoading"/>
+  </Transition>
 </template>
+
+<style scoped lang="scss">
+  .loader-enter-active,
+  .loader-leave-active {
+    transition: opacity 0.3s ease-in-out;
+  }
+
+  .loader-enter-from,
+  .loader-leave-to {
+    opacity: 0;
+  }
+</style>
---
+++ web/app/components/layout/loader/Loader.vue
@@ -0,0 +1,109 @@
+<script setup lang="ts">
+
+
+</script>
+
+<template>
+  <div 
+    class="
+      fixed top-0 left-0 w-full h-full z-100
+      flex items-center justify-center
+      bg-overlay
+    "
+    
+  >
+    <div class="flex items-center justify-center">
+      <div class="lds-spinner">
+        <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+  .lds-spinner {
+    color: currentColor;
+    display: inline-block;
+    position: relative;
+    width: 80px;
+    height: 80px;
+
+    & div {
+      transform-origin: 40px 40px;
+      animation: lds-spinner 1.2s linear infinite;
+
+      &::after {
+        content: " ";
+        display: block;
+        position: absolute;
+        top: 3.2px;
+        left: 36.8px;
+        width: 6.4px;
+        height: 17.6px;
+        border-radius: 20%;
+        background: var(--color-main);
+      }
+
+      &:nth-child(1) {
+        transform: rotate(0deg);
+        animation-delay: -1.1s;
+      }
+      &:nth-child(2) {
+        transform: rotate(30deg);
+        animation-delay: -1s;
+      }
+      &:nth-child(3) {
+        transform: rotate(60deg);
+        animation-delay: -0.9s;
+      }
+      &:nth-child(4) {
+        transform: rotate(90deg);
+        animation-delay: -0.8s;
+      }
+      &:nth-child(5) {
+        transform: rotate(120deg);
+        animation-delay: -0.7s;
+      }
+      &:nth-child(6) {
+        transform: rotate(150deg);
+        animation-delay: -0.6s;
+      }
+      &:nth-child(7) {
+        transform: rotate(180deg);
+        animation-delay: -0.5s;
+      }
+      &:nth-child(8) {
+        transform: rotate(210deg);
+        animation-delay: -0.4s;
+      }
+      &:nth-child(9) {
+        transform: rotate(240deg);
+        animation-delay: -0.3s;
+      }
+      &:nth-child(10) {
+        transform: rotate(270deg);
+        animation-delay: -0.2s;
+      }
+      &:nth-child(11) {
+        transform: rotate(300deg);
+        animation-delay: -0.1s;
+      }
+      &:nth-child(12) {
+        transform: rotate(330deg);
+        animation-delay: 0s;
+      }
+    }
+  }
+  @keyframes lds-spinner {
+    0% {
+      opacity: 1;
+    }
+    100% {
+      opacity: 0;
+    }
+  }
+
+
+
+</style>
