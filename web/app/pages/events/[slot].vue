f6b6870fe867d0ff74f6bb50d972d9acddc9082d
Vadim Kurenev <160814841+Vadim0-0@users.noreply.github.com>
2026-07-16 21:41:07 +0500

    feat(web): add events slot page scaffold

---
+++ web/app/pages/events/[slot].vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+  definePageMeta({
+    layout: 'main',
+    requiresAuth: true,
+  })
+
+</script>
+
+<template>
+  <section
+    class="
+      relative flex flex-col flex-1
+      bg-fourth
+    "
+  >
+
+  </section>
+</template>
+
+<style scoped lang="scss">
+
+</style>
