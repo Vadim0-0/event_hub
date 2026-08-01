ca47fd2b458d2ecc6097ca409b0c09e729347184
Vadim Kurenev <160814841+Vadim0-0@users.noreply.github.com>
2026-08-01 13:50:59 +0500

    fix(api): pass CORS allow_headers as list

--- api/app/main.py
+++ api/app/main.py
@@ -26,7 +26,7 @@ app.add_middleware(
   allow_origins = get_settings().cors_origins,
   allow_credentials = True,
   allow_methods = ["*"],
-  allow_headers = {"*"},
+  allow_headers = ["*"],
 )
 
 app.include_router(auth.router)
